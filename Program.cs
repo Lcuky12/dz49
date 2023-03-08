c56bd2b4a2622e3d70335b0cfd39329f86a48c00
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-08 20:09:24 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string password = "123";
+            int attempts = 3;
+            int unit = 1;
+
+            for (int i = 0; i < attempts; i++)
+            {
+                Console.Write(" Введите пароль ");
+                userInput = Console.ReadLine();
+               
+                if (userInput == password)
+                {
+                    Console.WriteLine(" Секретный код 564 ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" осталось - " + (attempts - i - unit) + " попыток ");
+                }
+            }  
+
+        }
+    }
+}
